0797364c0fa032b18e76f062d602b2227d4fb7df
TalayT <taalaytashkulov@gmail.com>
2021-10-03 12:44:20 -0400

    updated Third file

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -4,5 +4,10 @@ public class Third {
     public static void main(String[] args) {
         System.out.println("Hello Universe");
 
+        // adding new line
+
+        // adding second new file
+
+        // adding third new file
     }
 }
